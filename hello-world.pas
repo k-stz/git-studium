9ff3de2ba241600d5e534bb7ff0934769d2a0921
k-stz@thalassa <keistzen@gmail.com>
2016-02-12 21:42:43 +0100

    init commit

---
+++ hello-world.pas
@@ -0,0 +1,23 @@
+(* Author: k-stz *)
+
+Program Hi_Program;
+
+Procedure Test;
+{yep, this is comment syntax}
+Begin
+   writeln('test called, son.');
+End;
+
+(* this is a comment  
+   more here
+  *)
+
+{
+ and this is the same
+}
+
+Begin
+   Writeln('Hello World!!!');
+   Test; 
+end.
+
