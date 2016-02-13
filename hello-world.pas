85254263a50e58c36311e0be685d8c9de78b319a
k-stz@thalassa <keistzen@gmail.com>
2016-02-13 16:17:34 +0100

    predifined types in pascal

--- hello-world.pas
+++ hello-world.pas
@@ -1,5 +1,9 @@
 (* Author: k-stz *)
 
+{ Pascal ist eine typisierte Programmiersprache, da jede Variable,
+  vor der Benutzung, einem eindeutigen Datentyp zugeordnet werden
+  muss. en  }
+
 Program Hi_Program;
 
 Procedure Test1;
@@ -8,16 +12,24 @@ Begin
    writeln('test called, son.');
 End;
 
-(* this is a comment  
-   more here
-  *)
-
-{
- and this is the same
-}
 
 Begin
-   Writeln('Hello World!!!');
-   Test; 
+   Writeln('maxinteger:'); {<- 'strings'}
+   Writeln(succ(3) - pred(1));
+   Writeln(exp(ln(10))); {==> 1.0}
+   Writeln('Char:');
+   Writeln(succ('a'));	 {SUCC and PRED are also defined for CHARs}
+{ORD returns the ordinals for the char, CHAR returns denoted by the ordinat}
+   Writeln(char(ord('t')));
+   {Boolean: }
+   Writeln('Boolean:');
+   Writeln(true and false or not false ); {1 ∧ 0 ∨ ¬0}
+   Writeln('1=1 and true != false:');
+   Writeln((1 = 1) and true <> false);
+   Writeln('1 <= 12:');
+   Writeln(1 <= 12);
+   Writeln(1 <> 1);
+
+   { Test1;  }
 end.
 
