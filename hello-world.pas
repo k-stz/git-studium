33fe361d23c93e47bda1fa1d89aa7abfff091792
k-stz@thalassa <keistzen@gmail.com>
2016-02-14 16:35:38 +0100

    programming style guide and its actualisation

--- hello-world.pas
+++ hello-world.pas
@@ -1,61 +1,98 @@
 (* Author: k-stz *)
 
+{Style guide:
+   must have:
+     - Identifiers _must_ be written in CamelCase (think Java),
+     - CONSTANT identifiers _must_ be written in uppercase
+     - tType identifiers must start with a 't' (tWeek, tWeekend)
+     - begin
+         between Begin and End
+         we indent by 2 or
+           4 spaces, but 
+           consitently 
+       end 
+     - In if-then-else controll structures, the begin/end share the
+       same column:
+       
+       if <predicate> then
+       begin
+         <consequence>
+       end
+       else
+       begin
+         <alternative>
+       end;
+     
+     - program indent (input, output);
+         const
+         UPPERCASE = 'SAME COLUMN AS const!';
+         // same for type and var declaration/definition blocks
+
+       begin
+       end. /* indent */ <- seems like we write the name of procedure/ function at the end
+     
+     - comments: in the program headline you must write down what the program does (not
+                 how it does it). Usually you _must_ comment the input and output data. 
+                 TODO: what about procedures and functions?
+}
+
 { Pascal ist eine typisierte Programmiersprache, da jede Variable,
   vor der Benutzung, einem eindeutigen Datentyp zugeordnet werden
   muss. }
 
 {Program: <program heading> + <block> + . }
 
+{ The point of this program is its implementation, a collection of concepts and idea
+  of the programming language Pascal }
+program HiProgram; {<- this is the whole "program heading". What follows, all
+		       the way to the 'End.' at the very end is the <block>, minus
+		       the dot '.' (the dot in 'End.')}
 
-Program Hi_Program; {<- this is the whole "program heading". What follows, all
-		    the way to the 'End.' at the very end is the <block>, minus
-		    the dot '.' (the dot in 'End.')}
-
-var
-   x : integer;
+  var
+  x : integer;
 
 procedure IsOdd (input : integer);
-Begin
+begin
    write(input);
    if (odd(input)) then
-      writeln(' is odd') {<- careful, no semicolon here!}
+     writeln(' is odd') {<- careful, no semicolon here!}
    else
-      writeln(' is even');
-End;
+     writeln(' is even');
+end { IsOdd };
 
 
-Procedure Test1;
+procedure Test1;
 {declaration part:   <constant definition part>
                    + <type definition part>
                    + <variable declaration part> }
 
 { It seems between Begin and End variable definition is forbidden }
 
-const PI2 = sqr(pi); {constants should be written in _uppercase_! (style guide)}
+  const PI2 = sqr(pi); {constants should be written in _uppercase_! (style guide)}
 
 
-{ type definition part, comes aptly before the variable one,
-  as those will be used for variables! }
+  { type definition part, comes aptly before the variable one,
+    as those will be used for variables! }
 
-(* these also have an inner order such that we can compare them
-   and yield ORD(<tWeek var>) from them *)
-type tWeek = (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday);
+  (* these also have an inner order such that we can compare them
+     and yield ORD(<tWeek var>) from them *)
+  type tWeek = (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday);
 
-{ dt. Ausschnittstyp, en. /subrange type/ }
+  { dt. Ausschnittstyp, en. /subrange type/ }
   tWeekend = Saturday .. Sunday;
 
 
 { "variable declaration part" }
-var
-   age,
-   money : integer; 	{age and money both get the type integer here} 
-   day : tWeek;							       
-   weekend : tWeekend;
+  var
+  age,
+  money : integer; 	{age and money both get the type integer here} 
+  day : tWeek;							       
+  weekend : tWeekend;
 
 {/declaration part }
 
 {statement part: 'Begin'-label + <statement part> + 'End'-label}
-Begin
+begin
    { writeln('pi2:'); }
    { Writeln(pi2); }
    { Writeln('How old are you'); }
@@ -72,33 +109,33 @@ Begin
    writeln(weekend);  
    weekend := Sunday;	   {we can use the enumerator elements explicitly though!}
    writeln(weekend);
-End;
+end { Test1 };
 {/statement part }
 
 
-Begin
-   Writeln('maxinteger:'); {<- 'strings'}
-   Writeln(succ(3) - pred(1));
-   Writeln(exp(ln(10))); {==> 1.0}
-   Writeln('Char:');
-   Writeln(succ('a'));	 {SUCC and PRED are also defined for CHARs}
-{ORD returns the ordinals for the char, CHAR returns denoted by the ordinat}
-   Writeln(char(ord('t')));
+begin
+   writeln('maxinteger:'); {<- 'strings'}
+   writeln(succ(3) - pred(1));
+   writeln(exp(ln(10))); {==> 1.0}
+   writeln('Char:');
+   writeln(succ('a'));	 {SUCC and PRED are also defined for CHARs}
+   {ORD returns the ordinals for the char, CHAR returns denoted by the ordinat}
+   writeln(char(ord('t')));
    {Boolean: }
-   Writeln('Boolean:');
-   Writeln(true and false or not false ); {1 ∧ 0 ∨ ¬0}
-   Writeln('1=1 and true != false:',(1 = 1) and true <> false);
-   Writeln('1 <= 12:',1 <= 12,1 <> 1);
+   writeln('Boolean:');
+   writeln(true and false or not false ); {1 ∧ 0 ∨ ¬0}
+   writeln('1=1 and true != false:',(1 = 1) and true <> false);
+   writeln('1 <= 12:',1 <= 12,1 <> 1);
 
    x := 10;
    writeln(x);
-   Begin (* not a new namespace or lexical scope *)
+   begin (* not a new namespace or lexical scope *)
       x := 22;
       writeln(x);
-   End;
+   end;
    (* x is still 22 here *)
 
    Test1;
    IsOdd(10);
-End. 
+end { HiProgram }. 
 
