17533dea477bbec07626225e018dfe4ed9491cd6
k-stz@thalassa <keistzen@gmail.com>
2016-02-21 21:41:59 +0100

    record type example

--- data-structures.pas
+++ data-structures.pas
@@ -3,30 +3,48 @@ program dataStructures;
    type
       tNumberName  = (zero, one, two, three, four, five, six, seven, eight, nine, ten);
       tMatrixIndex = 0 .. 3;
+      {dt. "Verbundtyp"  }
+      tStudent =  record 
+		     name : string;
+		     age  : integer;
+		  end;		 {tStudent}
+			 
 
    var
       { array[<index type>] of <component type> }
-      names :  array[integer] of string; 
+      Names :  array[integer] of string; 
       { a multidimensional array's index type must be an ordinal type! }
-      identityMatrix : array[tMatrixIndex, tMatrixIndex] of integer;
-      numbers : array[integer] of tNumberName;
+      IdentityMatrix : array[tMatrixIndex, tMatrixIndex] of integer;
+      Numbers : array[integer] of tNumberName;
       i : integer;
+      { record }
+      BillyBob : tStudent;
+
+
 
 begin 
-   names[0] := 'minkowski';
+   Names[0] := 'minkowski';
    writeln(names[0]);
 
    { init the "numbers" array: }
    for i := 0 to 10 do
-      numbers[i] := tNumberName(i);
+      Numbers[i] := tNumberName(i);
 
    { count down }
    for i := 10 downto 0 do
-      writeln(numbers[i]);
+      writeln(Numbers[i]);
 
 
    { multidimensional arrays: }
    writeln('reading from multidimensional array -----:');
-   identityMatrix[0][0] := 1;
+   IdentityMatrix[0][0] := 1;
    writeln(identityMatrix[0][0]);
+
+   writeln('record -----:');
+   { as always, dot notation is used to access the slots }
+   BillyBob.name := 'Birry Bobu';
+   BillyBob.age := 5;
+   writeln('What''s your name little fella?',
+	   ' Name''s ', BillyBob.name, ' and I am ',
+	   BillyBob.age, ' years old!');
 end.
