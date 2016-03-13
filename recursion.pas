392ad0f5639aee2c852278a8c33b29e1b496029a
k-stz@thalassa <keistzen@gmail.com>
2016-03-13 09:31:21 +0100

    compute factorial with an interative process

--- recursion.pas
+++ recursion.pas
@@ -13,9 +13,29 @@ begin
       Fac := inN * Fac(inN - 1)
 end;  { Fac }
 
+function FacIter (inN :  integer) : integer;
+{ Compute factorial with a function generating an interative process. }
+
+   { Yes, this function can be only seen within FacIter, not in the body
+   of the "Recursion" program.  }
+   function Iter (inN : integer; product : integer) : integer;
+   { Auxiliary function hiding the state variable "product" from the API of
+     FacIter }
+   begin
+      if (inN > 0) then
+	 Iter := Iter (inN-1, product * inN)
+      else
+	 Iter := product;
+   end;	{ Iter }
+
+begin
+   FacIter := Iter(inN, 1);
+end;  { FacIter }
+
 begin
    writeln('Oh, remember: Pascal''s Integer can''t handle big numbers. ',
 	   'Fac(7) is the limit, lol');
-   writeln(Fac(1));
+   writeln(Fac(7));
 
+   writeln(FacIter(7));
 end.  { Recursion }
