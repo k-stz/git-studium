235eaad94635b0185bce5396d65ee6ec4bff6717
k-stz@thalassa <keistzen@gmail.com>
2016-04-11 23:58:28 +0200

    solution max-in-array

--- pascal/assignment2/max-in-array.pas
+++ pascal/assignment2/max-in-array.pas
@@ -1,4 +1,6 @@
 program MaxInArray;
+{ shows several statements that use the function MAX and we shall
+  decedide which could get the index out of bounds in the FELD array }
 
    const
    GRENZE = 10;
@@ -30,4 +32,34 @@ function max (
 
 begin
 
-end.
+   Feld[1] := 1; Feld[2] := 2; Feld[3] := 3; Feld[4] := 4; Feld[5] := 5;
+   Feld[6] := 6; Feld[7] := 7; Feld[8] := 88; Feld[9] := 9; Feld[10] := 10;
+   { A }
+   w := max (Feld, Feld[1], Feld[GRENZE]);
+   writeln(w);
+   { B }
+   w := max (Feld, (GRENZE-1) div 2,
+            (GRENZE-1) div 2);
+   writeln(w);
+   { C }
+   if max (Feld, 1, (GRENZE-1) div 2) >
+      max (Feld, (GRENZE+1) div 2, GRENZE)
+      then
+      w := max (Feld, 1, (GRENZE-1) div 2)
+   else
+      w := max (Feld, (GRENZE+1) div 2, GRENZE);
+   writeln(w);
+   { D - out of bounds}
+   w := max (Feld, 1, GRENZE);
+   if w <= GRENZE then { would fail if w <= 0}
+      write (max (Feld, w, w));
+   { E - out of bounds }
+   w1 := max (Feld, 1, GRENZE);
+   w2 := max (Feld, 4, GRENZE-1);
+   if (0 < w2) and (w1 <= GRENZE) then
+   begin { e.g. when w assumes a negative value }
+      w := max (Feld, 2, GRENZE);
+      w := max (Feld, 1, w)
+   end;
+
+end. { MaxInArray }
