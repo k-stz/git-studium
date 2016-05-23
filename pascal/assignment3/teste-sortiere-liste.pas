35a629a233aa9ed022603b61d6e57b8fea7daa10
k-stz@thalassa <keistzen@gmail.com>
2016-05-23 03:42:33 +0200

    muss regel 4 einrückung korrigiert; new(tmp) überflüssig!

--- pascal/assignment3/teste-sortiere-liste.pas
+++ pascal/assignment3/teste-sortiere-liste.pas
@@ -19,64 +19,63 @@ program TesteSortiereListe(input, output);
      Zeiger : tRefListe; { das letzte sortierte Element }
      Eingefuegt : boolean;
 
-     { Im folgenden heißt "Element" immer das erste Element aus dem unsortieren Listen Teil. }
-     begin
-	{ base case - die Leere Liste }
-	if (ioRefListe <> nil) then
+  { Im folgenden heißt "Element" immer das erste Element aus dem unsortieren Listen Teil. }
+  begin
+    { base case - die Leere Liste }
+    if (ioRefListe <> nil) then
+    begin
+      { init }
+      Zeiger := ioRefListe;
+      while (Zeiger^.next <> nil) do
+      begin
+	{ kleinstes Element? -> hänge an den Anfang der Liste}
+	if(ioRefListe^.info >= Zeiger^.next^.info) then
 	begin
-	   { init }
-	   Zeiger := ioRefListe;
-	   new(tmp);
-	   while (Zeiger^.next <> nil) do
-	   begin
-	      { kleinstes Element? -> hänge an den Anfang der Liste}
-	      if(ioRefListe^.info >= Zeiger^.next^.info) then
+	  { Verweis auf Folgeelement wird zwischengespeichert }
+	  tmp := Zeiger^.next^.next;
+	  { Element wird an Anfang der Liste gehangen }
+	  Zeiger^.next^.next := ioRefListe;
+	  { Element wird zum neuen Anfang der Liste }
+	  ioRefListe := Zeiger^.next;
+	  { Letztes sortiertes Element zeigt nun auf den Nachfolger des Elements bevor es
+	  umgehangenen wurde, d.h. das nächste unsortierte Element. }
+	  Zeiger^.next := tmp;
+	end
+        else { nicht kleinstes Element}
+	begin
+	  { Element ist das größte? -> hänge ans ende der sortierten Liste}
+	  if (Zeiger^.info <= Zeiger^.next^.info) then
+	  begin
+	    { easy, Element einfach Vorne belassen und es zum neuen vordersten Zeiger machen. }
+	    Zeiger := Zeiger^.next;
+	  end
+	  else
+	  begin
+	    { Element ist weder das kleinste noch das größte, jetzt suchen wir nach einer Stelle,
+	    in der sortierten Liste, um es einzuhängen. }
+	    Eingefuegt := false;
+	    iterZeiger := ioRefListe;
+	    while (not Eingefuegt) do
+	    begin
+	      if (iterZeiger^.next^.info >= Zeiger^.next^.info) then
 	      begin
-		 { Verweis auf Folgeelement wird zwischengespeichert }
-		 tmp := Zeiger^.next^.next;
-		 { Element wird an Anfang der Liste gehangen }
-		 Zeiger^.next^.next := ioRefListe;
-		 { Element wird zum neuen Anfang der Liste }
-		 ioRefListe := Zeiger^.next;
-		 { Letztes sortiertes Element zeigt nun auf den Nachfolger des Elements bevor es
-		 umgehangenen wurde, d.h. das nächste unsortierte Element. }
-		 Zeiger^.next := tmp;
-	      end
-	      else { nicht kleinstes Element}
-	      begin
-		 { Element ist das größte? -> hänge ans ende der sortierten Liste}
-		 if (Zeiger^.info <= Zeiger^.next^.info) then
-		 begin
-		    { easy, Element einfach Vorne belassen und es zum neuen vordersten Zeiger machen. }
-		    Zeiger := Zeiger^.next;
-		 end
-	         else
-		 begin
-		    { Element ist weder das kleinste noch das größte, jetzt suchen wir nach einer Stelle,
-		    in der sortierten Liste, um es einzuhängen. }
-		    Eingefuegt := false;
-		    iterZeiger := ioRefListe;
-		    while (not Eingefuegt) do
-		    begin
-		       if (iterZeiger^.next^.info >= Zeiger^.next^.info) then
-		       begin
-			  tmp := Zeiger^.next^.next; {Folgeelement zwischenspeichern}
-			  { Element wird umgehangen }
-			  Zeiger^.next^.next := iterZeiger^.next; {sortierte liste  *klick* vorne}
-			  iterZeiger^.next := Zeiger^.next; {sortierte liste *klick* hinten}
-			  
-			  { letztes sortiertes Element zeigt nun auf das nächste unsortierte, das 
-			    wir vorher in tmp zwischengespeichert haben. }
-			  Zeiger^.next := tmp;
-			  Eingefuegt := true;
-		       end;
-		       iterZeiger := iterZeiger^.next;
-		    end;  {while (not Eingefuegt) }
-		 end;
+		tmp := Zeiger^.next^.next; {Folgeelement zwischenspeichern}
+		{ Element wird umgehangen }
+		Zeiger^.next^.next := iterZeiger^.next; {sortierte liste  *klick* vorne}
+		iterZeiger^.next := Zeiger^.next; {sortierte liste *klick* hinten}
+		
+		{ letztes sortiertes Element zeigt nun auf das nächste unsortierte, das 
+		wir vorher in tmp zwischengespeichert haben. }
+		Zeiger^.next := tmp;
+		Eingefuegt := true;
 	      end;
-	   end; { while (Zeiger^.next <> nil) }
-	end; { base case - die leere Liste}
-     end; { SortiereListe }
+	      iterZeiger := iterZeiger^.next;
+	    end;  {while (not Eingefuegt) }
+	  end;
+	end;
+      end; { while (Zeiger^.next <> nil) }
+    end; { base case - die leere Liste}
+  end; { SortiereListe }
 
 procedure Anhaengen(var ioListe : tRefListe;
                         inZahl : tNatZahl);
